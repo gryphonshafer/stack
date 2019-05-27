a61ae22c299c492116e7958ea521ada20f5695f8
Gryphon Shafer <gryphon@goldenguru.com>
2019-05-27 15:52:51 -0700

    Shorewall automatic blocking based on error logs

---
+++ ansible/roles/services/postfix/files/shorewall.blacklist_auto.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+zgrep -E '\[sasl\] Ban ' /var/log/fail2ban.log* | \
+    perl -ne '/(\d+\.\d+\.\d+\.\d+)/; print "$1\n"' | sort | uniq -c | sort -nr \
+    > /tmp/fail2ban_repeaters
+
+cat /tmp/fail2ban_repeaters | \
+    perl -ne '/(\d+)\s+(\d+\.\d+\.\d+\.\d+)/; print ( ( $1 >= 0 ) ? "$2\n" : "null\n" )' | \
+    grep -v null | sort | \
+    perl -ne 'chomp; print "DROP net:$_ all\n"' \
+    > /tmp/shorewall_blacklist_auto
+
+cat /etc/shorewall/rules.d/blacklist_auto /tmp/shorewall_blacklist_auto | sort | uniq \
+    > /tmp/shorewall_blacklist_auto_sort_uniq
+
+cat /tmp/shorewall_blacklist_auto_sort_uniq > /etc/shorewall/rules.d/blacklist_auto
+
+rm \
+    /tmp/fail2ban_repeaters \
+    /tmp/blacklist_auto_new \
+    /tmp/shorewall_blacklist_auto \
+    /tmp/shorewall_blacklist_auto_sort_uniq
